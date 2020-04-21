0f072ecc0233c561876eb5e6247cde82de1cee9f
saadati944 <saadati944@gmail.com>
2020-04-21 19:00:01 +0430

    Add two controls for width and height.

--- random matrix/frmMain.Designer.cs
+++ random matrix/frmMain.Designer.cs
@@ -45,10 +45,17 @@
             this.label5 = new System.Windows.Forms.Label();
             this.numLineWidth = new System.Windows.Forms.NumericUpDown();
             this.label6 = new System.Windows.Forms.Label();
+            this.cd = new System.Windows.Forms.ColorDialog();
+            this.numHeight = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.numWidth = new System.Windows.Forms.NumericUpDown();
+            this.label8 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numCount)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numCons)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numPointSize)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numLineWidth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numWidth)).BeginInit();
             this.SuspendLayout();
             // 
             // btnDraw
@@ -104,9 +111,19 @@
             this.numCount.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.numCount.Location = new System.Drawing.Point(82, 10);
+            this.numCount.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             this.numCount.Name = "numCount";
             this.numCount.Size = new System.Drawing.Size(295, 20);
             this.numCount.TabIndex = 5;
+            this.numCount.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             // 
             // lblLog
             // 
@@ -139,7 +156,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(10, 124);
+            this.label3.Location = new System.Drawing.Point(11, 190);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(61, 13);
             this.label3.TabIndex = 9;
@@ -148,7 +165,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(11, 156);
+            this.label4.Location = new System.Drawing.Point(12, 222);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(60, 13);
             this.label4.TabIndex = 10;
@@ -159,20 +176,24 @@
             this.clrPointColor.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.clrPointColor.BackColor = System.Drawing.Color.Black;
-            this.clrPointColor.Location = new System.Drawing.Point(82, 124);
+            this.clrPointColor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.clrPointColor.Location = new System.Drawing.Point(83, 190);
             this.clrPointColor.Name = "clrPointColor";
             this.clrPointColor.Size = new System.Drawing.Size(295, 20);
             this.clrPointColor.TabIndex = 11;
+            this.clrPointColor.Click += new System.EventHandler(this.clrLineColor_Click);
             // 
             // clrLineColor
             // 
             this.clrLineColor.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.clrLineColor.BackColor = System.Drawing.Color.Black;
-            this.clrLineColor.Location = new System.Drawing.Point(82, 154);
+            this.clrLineColor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.clrLineColor.Location = new System.Drawing.Point(83, 220);
             this.clrLineColor.Name = "clrLineColor";
             this.clrLineColor.Size = new System.Drawing.Size(295, 20);
             this.clrLineColor.TabIndex = 12;
+            this.clrLineColor.Click += new System.EventHandler(this.clrLineColor_Click);
             // 
             // numPointSize
             // 
@@ -197,9 +218,19 @@
             this.numLineWidth.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.numLineWidth.Location = new System.Drawing.Point(82, 93);
+            this.numLineWidth.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             this.numLineWidth.Name = "numLineWidth";
             this.numLineWidth.Size = new System.Drawing.Size(295, 20);
             this.numLineWidth.TabIndex = 16;
+            this.numLineWidth.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             // 
             // label6
             // 
@@ -210,11 +241,81 @@
             this.label6.TabIndex = 15;
             this.label6.Text = "lines width :";
             // 
+            // numHeight
+            // 
+            this.numHeight.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numHeight.Location = new System.Drawing.Point(82, 155);
+            this.numHeight.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numHeight.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numHeight.Name = "numHeight";
+            this.numHeight.Size = new System.Drawing.Size(295, 20);
+            this.numHeight.TabIndex = 20;
+            this.numHeight.Value = new decimal(new int[] {
+            1080,
+            0,
+            0,
+            0});
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(11, 157);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "height :";
+            // 
+            // numWidth
+            // 
+            this.numWidth.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numWidth.Location = new System.Drawing.Point(82, 126);
+            this.numWidth.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numWidth.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numWidth.Name = "numWidth";
+            this.numWidth.Size = new System.Drawing.Size(295, 20);
+            this.numWidth.TabIndex = 18;
+            this.numWidth.Value = new decimal(new int[] {
+            1920,
+            0,
+            0,
+            0});
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(11, 128);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "width :";
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numHeight);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.numWidth);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.numLineWidth);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.numPointSize);
@@ -238,6 +339,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.numCons)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numPointSize)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numLineWidth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numWidth)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -262,6 +365,11 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.NumericUpDown numLineWidth;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ColorDialog cd;
+        private System.Windows.Forms.NumericUpDown numHeight;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown numWidth;
+        private System.Windows.Forms.Label label8;
     }
 }
 
--- random matrix/frmMain.cs
+++ random matrix/frmMain.cs
@@ -16,5 +16,10 @@ namespace random_matrix
         {
             InitializeComponent();
         }
+
+        private void clrLineColor_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
