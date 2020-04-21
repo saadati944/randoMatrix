db3d8bcac09dc33471a03dc2943b0165c7245a92
saadati944 <saadati944@gmail.com>
2020-04-21 20:03:28 +0430

    Stop drawing if user wants to stop it.

--- random matrix/frmMain.cs
+++ random matrix/frmMain.cs
@@ -50,25 +50,33 @@ namespace random_matrix
                 Graphics g = Graphics.FromImage(b as Image);
                 g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
 
+                if (stop) throw new Exception("user canseled.");
                 log($"set background color to {bcolor.ToString()}");
                 g.Clear(bcolor);
+                if (stop) throw new Exception("user canseled.");
 
                 Random r = new Random();
+                if (stop) throw new Exception("user canseled.");
 
                 log("create points list");
                 List<Point> ps = new List<Point>();
+                if (stop) throw new Exception("user canseled.");
                 for (int i = 0; i < points; i++)
                     ps.Add(new Point(r.Next(0, width), r.Next(0, height)));
+                if (stop) throw new Exception("user canseled.");
                 log($"{points} points created");
 
 
                 log("creating connections list");
                 List<Rectangle> ls = new List<Rectangle>();
+                if (stop) throw new Exception("user canseled.");
                 for (int i = 0; i < cons; i++)
                 {
+                    if (stop) throw new Exception("user canseled.");
                     //maybe all connections are avaliable so app shouldn`t hang.
                     for (int j = 0; j < 100; j++)
                     {
+                        if (stop) throw new Exception("user canseled.");
                         Point p1 = ps[r.Next(0, points)];
                         Point p2 = ps[r.Next(0, points)];
                         if (!ls.Contains(new Rectangle(p1.X, p1.Y, p2.X, p2.Y)) && !ls.Contains(new Rectangle(p2.X, p2.Y, p1.X, p1.Y)))
@@ -86,16 +94,22 @@ namespace random_matrix
                 Pen lp = new Pen(lcolor, lsize);
                 for (int i = 0; i < ls.Count; i++)
                 {
+                    if (stop) throw new Exception("user canseled.");
                     g.DrawLine(lp,ls[i].X,ls[i].Y, ls[i].Width, ls[i].Height);
                 }
                 log($"{ls.Count} lines drawed with this color : {lcolor.ToString()}");
 
+                if (stop) throw new Exception("user canseled.");
                 log("drawing points");
                 for (int i = 0; i < points; i++)
+                {
+                    if (stop) throw new Exception("user canseled.");
                     g.FillEllipse(new SolidBrush(pcolor), ps[i].X - psize / 2, ps[i].Y - psize / 2, psize, psize);
+                }
                 log($"points drawed with this color : {pcolor.ToString()}");
 
                 log("save file");
+                if (stop) throw new Exception("user canseled.");
                 b.Save(path);
                 log($"file saved in {path}");
             }
