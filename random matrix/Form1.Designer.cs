a1e2231296980f173704d3c66f531d5931e9aec7
saadati944 <saadati944@gmail.com>
2020-04-21 18:43:01 +0430

    Rename controls.

--- random matrix/Form1.Designer.cs
+++ random matrix/Form1.Designer.cs
@@ -29,9 +29,9 @@
         private void InitializeComponent()
         {
             this.btnDraw = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtSaveTo = new System.Windows.Forms.TextBox();
+            this.lblSaveTo = new System.Windows.Forms.Label();
+            this.lstLog = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // btnDraw
@@ -44,43 +44,43 @@
             this.btnDraw.Text = "Draw";
             this.btnDraw.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // txtSaveTo
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            this.txtSaveTo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(82, 417);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(625, 20);
-            this.textBox1.TabIndex = 1;
+            this.txtSaveTo.Location = new System.Drawing.Point(82, 417);
+            this.txtSaveTo.Name = "txtSaveTo";
+            this.txtSaveTo.Size = new System.Drawing.Size(625, 20);
+            this.txtSaveTo.TabIndex = 1;
             // 
-            // label1
+            // lblSaveTo
             // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(11, 420);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "save file to :";
+            this.lblSaveTo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblSaveTo.AutoSize = true;
+            this.lblSaveTo.Location = new System.Drawing.Point(11, 420);
+            this.lblSaveTo.Name = "lblSaveTo";
+            this.lblSaveTo.Size = new System.Drawing.Size(64, 13);
+            this.lblSaveTo.TabIndex = 2;
+            this.lblSaveTo.Text = "save file to :";
             // 
-            // listBox1
+            // lstLog
             // 
-            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.lstLog.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(374, 12);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(414, 394);
-            this.listBox1.TabIndex = 3;
+            this.lstLog.FormattingEnabled = true;
+            this.lstLog.Location = new System.Drawing.Point(391, 12);
+            this.lstLog.Name = "lstLog";
+            this.lstLog.Size = new System.Drawing.Size(397, 394);
+            this.lstLog.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lstLog);
+            this.Controls.Add(this.lblSaveTo);
+            this.Controls.Add(this.txtSaveTo);
             this.Controls.Add(this.btnDraw);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -92,9 +92,9 @@
         #endregion
 
         private System.Windows.Forms.Button btnDraw;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtSaveTo;
+        private System.Windows.Forms.Label lblSaveTo;
+        private System.Windows.Forms.ListBox lstLog;
     }
 }
 
